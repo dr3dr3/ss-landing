195449b17f238df21538d4ca65ccefc135463867
André Dreyer <git@andredreyer.com>
2023-10-29 22:20:47 +1000

    Changed to React with starter setup

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
         <div class="intro">Hello, I'm André</div>
         <div class="tagline">Sharing <a target="_blank" href="revealmd/index.html">stories</a> about DevOps and Product Management</div>
         <div class="icons-social">
-          <a target="_blank" href="https://github.com/dr3dr3">
+          <a href="https://github.com/dr3dr3">
                     <i class="fab fa-github" aria-hidden="true" title="Github"></i>
                     <span class="sr-only">Github</span>
                 </a>
-                <a target="_blank" href="https://linkedin.com/in/andredreyer">
+                <a href="https://linkedin.com/in/andredreyer">
                     <i class="fab fa-linkedin" aria-hidden="true" title="LinkedIn"></i>
                     <span class="sr-only">LinkedIn</span>
                 </a>
