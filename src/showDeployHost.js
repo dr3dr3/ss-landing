a07e2c3eccd5a0ddb44670ba88daacdc0ce4fb35
André Dreyer <git@andredreyer.com>
2023-11-12 12:00:41 +1000

    showHost component added context for feature flag

--- src/showDeployHost.js
+++ src/showDeployHost.js
@@ -2,16 +2,17 @@ import React from "react";
 import { useFlag, useStatus } from "@featurevisor/react";
 
 export default function ShowDeployHost() {
+  const context = { blueGreen: 'DEPLOY-BLUE-GREEN'};
   const { isReady } = useStatus();
-  const isEnabled = useFlag("showDeployHost");
+  const isEnabled = useFlag("showDeployHost", context);
 
   if (isReady && isEnabled) {
-    return <div className="host">Hosted on: DEPLOY-HOST-PLACEHOLDER</div>;
+    return <div className="host">Hosted on: DEPLOY-HOST-PLACEHOLDER 🟪</div>;
   }
 
   if (!isReady) {
     return <div className="host">...</div>;
   }
 
-  return <div className="host">🔖</div>;
+  return <div className="host">🟩</div>;
 };
